c575a7ce7eded8c09ec8868fc3b3f4a335621c6b
Trey Tacon <ttacon@gmail.com>
2019-12-20 17:41:43 -0500

    fix: correct the tag merge to preserve ordering

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_cloudwatch_log_group" "mixmax-log-group" {
   name              = var.name
   retention_in_days = var.retention_period
 
-  tags = merge(map("Environment", var.environment), var.custom_tags)
+  tags = merge(var.custom_tags, map("Environment", var.environment))
 }
