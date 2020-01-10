fb1bd38ea59647dcc220911a13fe10e16ee9f136
Ryan Tasson <ryant@mixmax.com>
2020-01-10 17:08:46 -0500

    feat: add log_group_name output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "log_group_arn" {
   description = "The ARN of the log group that we created."
   value       = aws_cloudwatch_log_group.mixmax-log-group.arn
 }
+
+output "log_group_name" {
+  description = "The name of the log group we created"
+  value = aws_cloudwatch_log_group.mixmax-log-group.name
+}
