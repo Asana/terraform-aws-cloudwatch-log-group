72bfbd550c1a2fca6cca8a2a2ad2b57e4be889fb
Eli Skeggs <eliskeggs@asana.com>
2021-07-01 11:15:33 -0700

    style: fix indentation

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_log_group" "mixmax-log-group" {
   retention_in_days = var.retention_period
 
   tags = merge(
-    var.custom_tags, 
+    var.custom_tags,
     {
       Environment = var.environment
       Service     = var.service
